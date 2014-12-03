05c0b587a2df1e76266fb29247a45fb67873a321
Andrej Lamov <andrej.lamov@gmail.com>
2014-12-03 12:53:12 +0100

    Rename argument in util

--- util.py
+++ util.py
@@ -1,10 +1,10 @@
 import os
 import subprocess as sp
 
-def gitDirExists(d):
+def gitDirExists(path):
     isGitRepo = not sp.call(['git', 'rev-parse', '--is-inside-work-tree'], stdout=sp.PIPE)
-    if os.path.isdir(d):
-        os.chdir(d)
+    if os.path.isdir(path):
+        os.chdir(path)
         return isGitRepo
     else:
         return False
