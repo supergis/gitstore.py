f483cf3ea6eb46317b856d5351f4c38f7adf8963
Andrej Lamov <andrej.lamov@gmail.com>
2014-12-07 00:02:36 +0100

    Resolve path to python scripts

--- gitstore.js
+++ gitstore.js
@@ -11,20 +11,23 @@ module.exports =
                 args.push('--' + k + '=' + flags[k]);
             }
         }
+        var resolvePath = function(cmd) {
+            return __dirname + '/' + cmd;
+        }
 
         return {
             init: function(dir, cb){
-                ef(python, ['init.py', dir], cb);
+                ef(python, [resolvePath('init.py'), dir], cb);
             },
 
             put: function(dir, file, data, flags, cb){
-                var args = ['put.py', dir, file, data]
+                var args = [resolvePath('put.py'), dir, file, data]
                 pushFlags(args, flags);
                 ef(python, args, cb);
             },
 
             get: function(dir, file, flags, cb){
-                var args = ['get.py', dir, file]
+                var args = [resolvePath('get.py'), dir, file]
                 pushFlags(args, flags);
                 ef(python, args, cb);
             },
