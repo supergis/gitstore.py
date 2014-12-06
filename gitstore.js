443051a4112c2b2e71b97d1dc4c43d5e42ce8719
Andrej Lamov <andrej.lamov@gmail.com>
2014-12-07 00:14:25 +0100

    Less code with general execute function

--- gitstore.js
+++ gitstore.js
@@ -1,6 +1,5 @@
 var ef = require('child_process').execFile;
 
-
 module.exports =
     (function(){
 
@@ -10,26 +9,25 @@ module.exports =
             for(k in flags) {
                 args.push('--' + k + '=' + flags[k]);
             }
-        }
-        var resolvePath = function(cmd) {
-            return __dirname + '/' + cmd;
-        }
+        };
+
+        var exec = function(cmd0, args, flags, callback){
+            cmd = [__dirname + '/' + cmd0].concat(args);
+            if(flags) pushFlags(cmd, flags);
+            ef(python, cmd, callback);
+        };
 
         return {
-            init: function(dir, cb){
-                ef(python, [resolvePath('init.py'), dir], cb);
+            init: function(dir, callback){
+                exec('init.py', [dir], [], callback)
             },
 
-            put: function(dir, file, data, flags, cb){
-                var args = [resolvePath('put.py'), dir, file, data]
-                pushFlags(args, flags);
-                ef(python, args, cb);
+            put: function(dir, file, data, flags, callback){
+                exec('put.py', [dir, file, data], flags, callback)
             },
 
-            get: function(dir, file, flags, cb){
-                var args = [resolvePath('get.py'), dir, file]
-                pushFlags(args, flags);
-                ef(python, args, cb);
+            get: function(dir, file, flags, callback){
+                exec('get.py', [dir, file], flags, callback)
             },
-        }
+        };
     })()
